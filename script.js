b24a8971727a35789c07a55de5d7cca244a9c808
KwameWilson <kwamidzorgbenyuie@gmail.com>
2024-01-31 17:24:08 +0000

    Finished the toggle button dark mode

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const lightMode = document.querySelector('.light-mode');
-const darkMode = document.querySelector('.dark-mode');
+// const lightMode = document.querySelector('.light-mode');
+// const darkMode = document.querySelector('.dark-mode');
+// const body = document.querySelector('body');
+
+// darkMode.addEventListener('click', () => {
+//     body.style.backgroundColor = '#121212';
+// })
+
+// lightMode.addEventListener('click', () => {
+//     body.style.backgroundColor = "#fff"
+// })
+
+
 const body = document.querySelector('body');
+const slider = document.querySelector('.slider')
 
-darkMode.addEventListener('click', () => {
-    body.style.backgroundColor = '#121212';
+
+toggle.addEventListener("click", () => {
+
+    if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode')
+    } else {
+        body.classList.add('dark-mode')
+    }
 })
 
-lightMode.addEventListener('click', () => {
-    body.style.backgroundColor = "#fff"
-})
